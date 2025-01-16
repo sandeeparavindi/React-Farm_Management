3b4e726d945152969e461346100489cf52af607b
sandeeparavindi <sandeeparavindi1022@gmail.com>
2025-01-16 17:19:38 +0530

    vehicle form update button border change

--- src/components/vehicle/VehicleFormComponent.tsx
+++ src/components/vehicle/VehicleFormComponent.tsx
@@ -220,7 +220,7 @@ export const VehicleFormComponent = () => {
                 <button
                     type="button"
                     onClick={() => handleVehicleOperation("UPDATE_VEHICLE")}
-                    className="w-full text-white bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    className="w-full text-white bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
                     Update Vehicle
                 </button>
