4f2657175280d7bc398d81379a6ddff199b0d9cc
sandeeparavindi <sandeeparavindi1022@gmail.com>
2025-01-16 19:06:21 +0530

    crop model

---
+++ src/models/crop.ts
@@ -0,0 +1,24 @@
+export class Crop {
+    crop_code: string;
+    common_name: string;
+    scientific_name: string;
+    category: string;
+    season: string;
+    crop_image: string;
+
+    constructor(
+        crop_code: string,
+        common_name: string,
+        scientific_name: string,
+        category: string,
+        season: string,
+        crop_image: string,
+    ) {
+        this.crop_code = crop_code;
+        this.common_name = common_name;
+        this.scientific_name = scientific_name;
+        this.category = category;
+        this.season = season;
+        this.crop_image = crop_image;
+    }
+}
