21df78dcabc43e24e8611aafd9f73c35326bde17
sandeeparavindi <sandeeparavindi1022@gmail.com>
2025-01-16 19:38:15 +0530

    crop in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import VehicleSlice from "../reducers/VehicleSlice.tsx";
 import FieldSlice from "../reducers/FieldSlice.tsx";
+import CropSlice from "../reducers/CropSlice.tsx";
 
 export const store = configureStore({
     reducer: {
         vehicle: VehicleSlice,
         field: FieldSlice,
+        crop: CropSlice,
     }
 });
