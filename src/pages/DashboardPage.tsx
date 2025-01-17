f1a8a62f7efbb6fb4ee25291b5e376f8038d226c
sandeeparavindi <sandeeparavindi1022@gmail.com>
2025-01-17 20:23:10 +0530

    staff cards navigate dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,10 +1,15 @@
 import {HeaderComponent} from "../components/sidebar/HeaderComponent.tsx";
+import {StaffCardComponent} from "../components/staff/StaffCardComponent.tsx";
 
 export const DashboardPage = () => {
 
     return (
         <>
-            <HeaderComponent section="Dashboard Section" />
+            <HeaderComponent section="Dashboard"/>
+            <div>
+                <h1 className="text-2xl font-bold text-blue-700">Staff Members</h1>
+                <StaffCardComponent/>
+            </div>
         </>
     );
 };
