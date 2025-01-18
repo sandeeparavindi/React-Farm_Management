9f42ea306c8c4d7ee7039e493520777cbe4008a7
sandeeparavindi <sandeeparavindi1022@gmail.com>
2025-01-18 16:57:22 +0530

    staff card change

--- src/components/staff/StaffCardComponent.tsx
+++ src/components/staff/StaffCardComponent.tsx
@@ -1,27 +1,60 @@
-import { useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {FaTrashAlt} from "react-icons/fa";
+import {deleteStaff} from "../../reducers/StaffSlice.tsx";
 
 export const StaffCardComponent = () => {
     const staffList = useSelector((state) => state.staff);
+    const dispatch = useDispatch();
 
+    const handleDelete = (staffId: string) => {
+        if (window.confirm(`Are you sure you want to delete equipment with ID: ${staffId}?`)) {
+            dispatch(deleteStaff(staffId));
+        }
+    };
     return (
         <div className="p-6 grid gap-6 md:grid-cols-3 lg:grid-cols-4">
             {staffList.map((staff) => (
                 <div
                     key={staff.staff_id}
-                    className="p-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow bg-white"
+                    className="flex flex-col justify-between p-4 border rounded-lg shadow-lg hover:shadow-2xl transition-shadow bg-white min-h-[350px]"
                 >
-                    <h2 className="text-lg font-semibold text-blue-600">
-                        {staff.first_name} {staff.last_name}
-                    </h2>
-                    <p className="text-sm text-gray-700">ID: {staff.staff_id}</p>
-                    <p className="text-sm text-gray-700">Designation: {staff.Designation}</p>
-                    <p className="text-sm text-gray-700">Gender: {staff.Gender}</p>
-                    <p className="text-sm text-gray-700">Joined: {staff.joined_date}</p>
-                    <p className="text-sm text-gray-700">DOB: {staff.data_of_birth}</p>
-                    <p className="text-sm text-gray-700">Mobile: {staff.mobile}</p>
-                    <p className="text-sm text-gray-700">
-                        Address: {staff.address_line1} {staff.address_line2} {staff.address_line3} {staff.address_line4} {staff.address_line5}</p>
-                    <p className="text-sm text-gray-700">Vehicle: {staff.vehicle}</p>
+                    <div>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>ID:</strong> {staff.staff_id}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>Designation:</strong> {staff.Designation}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>Gender:</strong> {staff.Gender}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>Joined:</strong> {staff.joined_date}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>DOB:</strong> {staff.data_of_birth}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>Mobile:</strong> {staff.mobile}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>Address:</strong> {`${staff.address_line1} ${staff.address_line2} ${staff.address_line3} ${staff.address_line4} ${staff.address_line5}`}
+                        </p>
+                        <p className="text-sm text-gray-700 mb-2">
+                            <strong>Vehicle:</strong> {staff.vehicle}
+                        </p>
+                    </div>
+                    <div className="mt-4">
+                        <h2 className="text-lg font-semibold text-blue-600 border-t pt-4 text-center">
+                            {staff.first_name} {staff.last_name}
+                        </h2>
+                        <button
+                            onClick={() => handleDelete(staff.staff_id)}
+                            className="mt-4 flex items-center justify-center min-w-[335px] text-white bg-red-500 hover:bg-red-700 font-medium rounded-lg text-sm px-4 py-2 transition-colors duration-300"
+                        >
+                            <FaTrashAlt className="mr-2"/> Delete
+                        </button>
+                    </div>
                 </div>
             ))}
         </div>
